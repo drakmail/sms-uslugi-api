a3b2208f5fd26adf4e664af30f94c400d7ccbc65
Alexander Maslov <drakmail@delta.pm>
2014-02-22 14:40:41 +0400

    Add faraday

--- lib/sms-uslugi-api.rb
+++ lib/sms-uslugi-api.rb
@@ -1,8 +1,14 @@
+require "faraday"
+
 class SmsUslugiApi
 
   def initialize(username, password)
     @username = username
     @password = password
+    @api = Faraday.new(url: "https://lcab.sms-uslugi.ru/lcabApi") do |faraday|
+      faraday.request :url_encoded
+      faraday.adapter Faraday.default_adapter
+    end
   end
 
 end
